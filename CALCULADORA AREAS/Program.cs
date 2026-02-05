035a0e648ff4c0229cac37eca119e04f0081f122
brayan <tovarromobrayangiovanni@gmail.com>
2026-02-04 21:03:05 -0600

    Primer Commit

--- CALCULADORA AREAS/Program.cs
+++ CALCULADORA AREAS/Program.cs
@@ -10,6 +10,28 @@ namespace CALCULADORA_AREAS
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("CALCULADORA DE AREAS");
+            Console.WriteLine("1. Area de un cuadrado");
+            Console.WriteLine("2. Area de un rectangulo");
+            Console.WriteLine("3. Area de un circulo");
+            Console.WriteLine("4. Area de un triangulo");
+            Console.WriteLine("5. Salir");
+            int opcion = 0;
+            while (opcion != 5)
+            {
+                Console.Write("Seleccione una opción: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Ingrese el lado del cuadrado: ");
+                        double lado = Convert.ToDouble(Console.ReadLine());
+                        double areaCuadrado = lado * lado;
+                        Console.WriteLine($"El área del cuadrado es: {areaCuadrado}");
+                        break;
+                   
+                }
+            }
         }
     }
 }
